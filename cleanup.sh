620b88330cee99a7906582096d870a26f092f298
nando p <fernando.pando@stelligent.com>
2015-12-06 14:22:25 -0500

    add cleanup

--- cleanup.sh
+++ cleanup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+supervisorctl shutdown
 find . -size 0 -print -delete
+ps -ef | grep vlc | awk '{print $2}' | xargs kill -9
--- make_record_cameras.rb
+++ make_record_cameras.rb
@@ -17,10 +17,10 @@ resolution = 4
 cycle_time = 3600
 
 cameras = eval(File.open(File.expand_path('load_camera_hash.rb')).read)
-cameras ||=
-  Hash[ 'camera-1' => "10.0.0.1",
-        'camera-2' => "10.0.0.2",
-        'camera-3' => "10.0.0.3",
+#cameras ||=
+#  Hash[ 'camera-1' => "10.0.0.1",
+#        'camera-2' => "10.0.0.2",
+#        'camera-3' => "10.0.0.3",
 
 class MakeSupdConf
   def initialize(cameras, template, supd_home, supd_prog_prefix, supd_sock, supd_pid, supd_log)
@@ -66,15 +66,32 @@ class MakeSupdProg
   end
 end
 
-
-supd_conf = MakeSupdConf.new(cameras, File.read(supd_conf_template), supd_home, supd_prog_prefix, supd_sock, supd_pid, supd_log)
+supd_conf =
+  MakeSupdConf.new(
+    cameras,
+    File.read(supd_conf_template),
+    supd_home,
+    supd_prog_prefix,
+    supd_sock,
+    supd_pid,
+    supd_log
+  )
 supd_conf.save(supd_conf_file)
 Dir.mkdir(data_dir) unless Dir.exist?(data_dir)
 
 cameras.each do | camera, ip | 
   camera_data_dir = "#{data_dir}/#{camera}"
   Dir.mkdir(camera_data_dir) unless Dir.exist?(camera_data_dir)
-  supd_prog = MakeSupdProg.new(ip, camera, File.read(supd_prog_template), supd_home, resolution, cycle_time, data_dir)
+  supd_prog =
+    MakeSupdProg.new(
+      ip,
+      camera,
+      File.read(supd_prog_template),
+      supd_home,
+      resolution,
+      cycle_time,
+      data_dir
+    )
   supd_prog.save("#{supd_prog_prefix}#{camera}.sh")
   File.chmod(0700, "#{supd_prog_prefix}#{camera}.sh")
 end
